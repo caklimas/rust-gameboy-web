da3462c12ae4990434277822564468e15b0a310b
Christopher Klimas <caklimas@gmail.com>
2020-11-15 17:35:02 -0500

    Remove warnings

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,18 +2,8 @@ import React from 'react';
 import './App.css';
 import Gameboy from '../Gameboy/Gameboy';
 import RomLoader from '../RomLoader/RomLoader';
-import { RustGameboy , loadWasm } from '../../helpers/wasm';
 
-interface AppState {
-  gameboy_pointer: number,
-  wasm: RustGameboy
-}
-
-class App extends React.Component<{}, AppState> {
-    constructor(props: {}) {
-        super(props);
-    }
-
+class App extends React.Component {
     render() {
         return (
             <div className='app'>
