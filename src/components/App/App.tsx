d1c8a4b9577b5a01427e3fd8c0aca7b532493b79
Christopher Klimas <caklimas@gmail.com>
2020-11-15 17:28:03 -0500

    Add back folders

---
+++ src/components/App/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import Gameboy from '../Gameboy/Gameboy';
+import RomLoader from '../RomLoader/RomLoader';
+import { RustGameboy , loadWasm } from '../../helpers/wasm';
+
+interface AppState {
+  gameboy_pointer: number,
+  wasm: RustGameboy
+}
+
+class App extends React.Component<{}, AppState> {
+    constructor(props: {}) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='app'>
+                <RomLoader />
+                <Gameboy />
+            </div>
+        );
+    }
+}
+
+export default App;
