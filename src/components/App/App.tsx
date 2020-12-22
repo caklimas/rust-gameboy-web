6656b8faa0847bf1b87119d04c1c77ff9a414fb3
caklimas <caklimas@gmail.com>
2020-12-21 20:34:09 -0500

    Test multitouch

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,84 @@
 import React from 'react';
 import './App.scss';
-import Gameboy from '../Gameboy/Gameboy';
-import RomLoader from '../RomLoader/RomLoader';
+// import Gameboy from '../Gameboy/Gameboy';
+// import RomLoader from '../RomLoader/RomLoader';
 
 class App extends React.Component {
     render() {
         return (
             <div className='app'>
-                <RomLoader />
-                <Gameboy />
+                {/* <RomLoader />
+                <Gameboy /> */}
+                <Multitouch id='target1' text='Tap, Hold or Swipe me 1' />
+                <Multitouch id='target2' text='Tap, Hold or Swipe me 2' />
+                <Multitouch id='target3' text='Tap, Hold or Swipe me 3' />
+                <Multitouch id='target4' text='Tap, Hold or Swipe me 4' />
             </div>
         );
     }
 }
+
+const Multitouch = (props: MultitouchProps) => {
+    return (
+        <div
+            id={props.id}
+            className='touch-none'
+            onTouchStart={
+                (e) => {
+                    e.preventDefault();
+                    updateBackground(e);
+                }
+            }
+            onTouchCancel={
+                (e: any) => {
+                    e.preventDefault();
+                    if (e.targetTouches.length === 0) {
+                        // Restore background and border to original values
+                        e.target.style.background = "white";
+                        e.target.style.border = "1px solid black";
+                    }
+                }
+            }
+            onTouchEnd={
+                (e: any) => {
+                    e.preventDefault();
+                    if (e.targetTouches.length === 0) {
+                        // Restore background and border to original values
+                        e.target.style.background = "white";
+                        e.target.style.border = "1px solid black";
+                    }
+                }
+            }
+        >
+            {props.text}
+        </div>
+    );
+};
+
+const updateBackground = (ev: any) => {
+    // Change background color based on the number simultaneous touches
+    // in the event's targetTouches list:
+    //   yellow - one tap (or hold)
+    //   pink - two taps
+    //   lightblue - more than two taps
+    switch (ev.targetTouches.length) {
+      case 1:
+        // Single tap`
+        ev.target.style.background = "yellow";
+        break;
+      case 2:
+        // Two simultaneous touches
+        ev.target.style.background = "pink";
+        break;
+      default:
+        // More than two simultaneous touches
+        ev.target.style.background = "lightblue";
+    }
+}
+
+interface MultitouchProps {
+    id: string,
+    text: string
+};
 
 export default App;
